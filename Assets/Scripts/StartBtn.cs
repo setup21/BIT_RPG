95afabd536cc91a51d4c0dbf86b4acec5eb98ec7
setup21 <setup218@gmail.com>
2021-10-26 22:00:38 +0900

    Loading_Scene Fixing
    10-26

--- Assets/Scripts/StartBtn.cs
+++ Assets/Scripts/StartBtn.cs
@@ -7,25 +7,16 @@ using UnityEngine.UI;
 
 public class StartBtn : MonoBehaviour
 {
-    GameObject Load_Scene;
-    GameObject Start_Button;
-    GameObject Game_Text;
-    public MenuManager MS;
+    [SerializeField] private GameObject Load_Scene;
+    [SerializeField] private GameObject Start_Button;
+    [SerializeField] private GameObject Game_Text;
+    [SerializeField] private GameObject Title_UI;
 
-    void Start()
-    {        
-        Load_Scene = GameObject.Find("LoadingImage");
-        Load_Scene.SetActive(false);
-        
-        Start_Button = GameObject.Find("StartButton");
-        Game_Text = GameObject.Find("Canvas");        
-    }
     public void Loading()
     {
         Load_Scene.SetActive(true);
-        Load_Scene.GetComponent<Canvas>().enabled = true;
         Start_Button.SetActive(false);
-        Game_Text.GetComponent<Canvas>().enabled = false;
+        Title_UI.SetActive(false);
         Invoke("GameStart", 3.5f);
     }
     public void GameStart()
--- Assets/Scripts/TitleTyping.cs
+++ Assets/Scripts/TitleTyping.cs
@@ -8,36 +8,16 @@ using System.Globalization;
 
 public class TitleTyping : MonoBehaviour
 {
-    GameObject Start_Button;
-    GameObject Load_Image;
-    GameObject Text;
+    [SerializeField] GameObject Start_Button;
+    [SerializeField] GameObject Load_Image;
+    [SerializeField] GameObject Text;
     public float typingSpeed;
     public string sentence;
-    /*public enum AnimationMode { VertexColor, Jitter};
-    public AnimationMode MeshAnimationMode = AnimationMode.Jitter;
-    private TextMeshPro m_TextMeshPro;
-    private TextContainer m_TextContainer;
-
-    private TextInfo m_textInfo;*/
-
-    /*private void Awake()
-    {
-        m_TextMeshPro = gameObject.AddComponent<TextMeshPro>();
-        m_TextMeshPro.text = Text.GetComponent<TextMeshPro>().text;
-        m_TextMeshPro.enableWordWrapping = true;
-        m_TextContainer = GetComponent<TextContainer>();
-        m_TextContainer.width = 10f;
-
-        m_TextMeshPro.ForceMeshUpdate();
-    }*/
 
     private void Start()
-    {        
-        Load_Image = GameObject.Find("LoadingImage");
-        Load_Image.GetComponent<Canvas>().enabled = false;
-        Start_Button = GameObject.Find("StartButton");
+    {
         Start_Button.SetActive(false);
-        Text = GameObject.Find("Title");
+        Load_Image.SetActive(false);
 
         Text.GetComponent<TextMeshProUGUI>().text = ""; 
         StartCoroutine(Type());
