fe131f1352e376adb32ba2bcf0e4e6dff592f0db
setup21 <setup218@gmail.com>
2022-04-01 18:39:21 +0900

    Update CameraMove.cs

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -14,20 +14,41 @@ public class CameraMove : MonoBehaviour
     public Vector2 maxPosition;
     public Vector2 minPosition;
 
-
-    // Start is called before the first frame update
-    void Start()
+    public void SetResoulution()
     {
-        Debug.Log(tilemap.cellBounds);
-        Debug.Log(tilemap.cellGap);
-        Debug.Log(tilemap.cellSize);
-        Debug.Log(tilemap.localBounds);
+        int setWidth = 1920;
+        int setHeight = 1080;
+
+        int deviceWidth = Screen.width;
+        int deviceHeight = Screen.height;
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+        }
+    }
+
+    private void Start()
+    {
+        SetResoulution();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        if(transform.position != Player.position)
+
+        SetResoulution();
+
+
+        if (transform.position != Player.position)
         {
             Vector3 PlayerPosition = new Vector3(Player.position.x, Player.position.y, transform.position.z);
 
